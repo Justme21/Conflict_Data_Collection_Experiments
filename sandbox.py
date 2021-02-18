b67d8c0f81eeb9b46229b51ce1cd7136189c3273
Jack Geary <Jack.Geary@ed.ac.uk>
2021-02-18 14:37:04 +0000

    Public Release 1.0

--- sandbox.py
+++ sandbox.py
@@ -177,7 +177,7 @@ def runExperiment():
     space_size = 15
 
     instructions = ["-Press and hold UP arrow to accelerate","-Press and hold DOWN arrow to decelerate","-Press and hold the LEFT arrow to turn anti-clockwise","-Press and hold RIGHT arrow to turn clockwise","-Press SPACE to pause simulation"]
-    write_instructions = writeText(screen,instructions,(0,int(h/3)),font_size,space_size)
+    write_instructions = writeText(screen,instructions,(0,int(h/5)),font_size,space_size)
    
     ###########################################################################################
     #Setting up Controllers for Lane Keeping Vehicle
